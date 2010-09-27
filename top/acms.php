f459e50d46be56a2a96c63ebc7fa8b944908f688
Marc Lewis <marc@innovotel.com>
2010-09-27 21:40:50 +0000

    Removed pconnect for database connections

--- top/acms.php
+++ top/acms.php
@@ -1,9 +1,29 @@
-<?
-/*
- * acms.php -  This is the main program body besides the index.
- *             It handles calling of modules and other things.
+<?php
+/**
+ * acms.php - Controller.  Hands off all functions to acmsApp.
  *
+ **************************************************************************
+ * Written by R. Marc Lewis, 
+ *   Copyright 2004-2010, R. Marc Lewis (marc@CheetahIS.com)
+ *   Copyright 2007-2010, Cheetah Information Systems Inc.
+ **************************************************************************
+ *
+ * This file is part of Another Content Management System (ACMS)
+ *
+ * ACMS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ACMS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ACMS.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 // Setup our include path.  We will only load things with a full path,
 // and only based on what the server tells us about.
 $libdir = substr($_SERVER['DOCUMENT_ROOT'], 0, strrpos($_SERVER['DOCUMENT_ROOT'], "/")) . "/lib/";
